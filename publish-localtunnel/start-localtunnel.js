5fbb73df815563e7d86e572dadf88a477646c902
acailly <ylliac14@gmail.com>
2020-07-02 15:13:17 +0200

    mise à disposition via localtunnel

--- publish-localtunnel/start-localtunnel.js
+++ publish-localtunnel/start-localtunnel.js
@@ -1,13 +1,16 @@
-const localtunnel = require('localtunnel');
+const localtunnel = require("localtunnel");
 
-const configuration = require('../configuration')
+const configuration = require("../configuration");
 
-module.exports = async function(){
-  const tunnel = await localtunnel({ port: configuration.applicationServerPort });
+module.exports = async function () {
+  const tunnel = await localtunnel({
+    port: configuration.applicationServerPort,
+    host: "http://serverless.social" // See https://github.com/localtunnel/localtunnel/issues/343
+  });
 
-  console.log(`The application is available at: ${tunnel.url}.localtunnel.me`)
+  console.log(`The application is available at: ${tunnel.url}`);
 
-  tunnel.on('close', () => {
-    console.log("Tunnel closed")
+  tunnel.on("close", () => {
+    console.log("Tunnel closed");
   });
 };
