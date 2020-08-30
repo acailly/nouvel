123cebd08e2c7ac309dcd304d4012532e6892162
acailly <ylliac14@gmail.com>
2020-08-30 14:12:00 +0200

    en cours : Nouvelle passe sur la synchronisation

--- start.js
+++ start.js
@@ -1,15 +1,15 @@
-const {get: getIdentity} = require('./identity')
+const { get: getIdentity } = require("./identity");
 const identity = getIdentity();
-console.log(`Connected as ${identity.id}`)
+console.log(`Connected as ${identity.id}`);
 
 const startApplicationExpress = require("./application-express/start-application-express");
 startApplicationExpress();
-
-const startSynchronizationGit = require("./synchronization-git/start-synchronization-git");
-startSynchronizationGit();
 
 const startPublishingGitDumbHttp = require("./publish-gitdumbhttp/start-gitdumbhttp");
 startPublishingGitDumbHttp();
 
 const startPublishingLocaltunnel = require("./publish-localtunnel/start-localtunnel");
 startPublishingLocaltunnel();
+
+const startSynchronizationGit = require("./synchronization-git/start-synchronization-git");
+startSynchronizationGit();
