32408d064c5a2144b6470ac1b03ac5bdd7d6ffba
acailly <ylliac14@gmail.com>
2020-11-14 13:31:55 +0100

    Deployer sur github pages

--- git-native/getRemoteUrl.js
+++ git-native/getRemoteUrl.js
@@ -1,7 +1,7 @@
 const execShellCommand = require("./execShellCommand");
 module.exports = async (folder, remoteName) => {
   try {
-    await execShellCommand(`git remote get-url ${remoteName}`, folder);
+    return await execShellCommand(`git remote get-url ${remoteName}`, folder);
   } catch (e) {
     return;
   }
