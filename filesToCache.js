c8e09b6f739cb7da95f338c91098bddca60ffead
acailly <ylliac14@gmail.com>
2020-11-14 14:16:35 +0100

    Updates

---
+++ filesToCache.js
@@ -0,0 +1,9 @@
+var filesToCache = [
+  "index.html",
+  "views/404.html",
+  "views/index.html",
+  "views/partials/head.html",
+  "views/repositories.html",
+  "tacit-css.min.css",
+  "bundle.js"
+]
