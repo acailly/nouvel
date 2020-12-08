cfbe401712e00517941852d82b0b702c1a10adf4
Antoine Cailly <ylliac14@gmail.com>
2020-12-08 17:03:52 +0100

    Tester la modification en même temps

--- git-isomorphic/somethingToCommit.js
+++ git-isomorphic/somethingToCommit.js
@@ -1,6 +1,17 @@
 const git = require("isomorphic-git");
 const fs = require("fs");
+const path = require("path");
 module.exports = async (folder) => {
-  const indexedFiles = await git.listFiles({ fs, dir: folder });
-  return indexedFiles && indexedFiles.length;
+  // Using wonderful command statusMatrix
+  // See more at https://isomorphic-git.org/docs/en/statusMatrix.html
+  const FILE = 0,
+    HEAD = 1,
+    WORKDIR = 2,
+    STAGE = 3;
+
+  const filenames = (await git.statusMatrix({ fs, dir: folder }))
+    .filter((row) => row[HEAD] !== row[STAGE])
+    .map((row) => row[FILE]);
+
+  return filenames && filenames.length;
 };
