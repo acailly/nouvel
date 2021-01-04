d028768ad276a36eef004ff7fded96c4f1267412
Antoine Cailly <ylliac14@gmail.com>
2021-01-04 09:11:20 +0100

    Faire marcher tout ca dans le browser

--- pouchdb-default/getPouchDBObject.js
+++ pouchdb-default/getPouchDBObject.js
@@ -1,6 +1,12 @@
 const PouchDB = require("pouchdb");
+const makeDir = require("make-dir");
+const fs = require("fs");
 const configuration = require("../@configuration");
 
+// TODO ACY Tester si ca marche dans le browser
+if (!fs.existsSync(configuration.localDatabaseFolder)) {
+  makeDir.sync(configuration.localDatabaseFolder);
+}
 // TODO ACY Tester si le prefix marche toujours dans le browser
 const LocalPouchDB = PouchDB.defaults({
   prefix: `${configuration.localDatabaseFolder}/`,
