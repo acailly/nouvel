b4a31db801f61f29898fbb1f26c6eae87eced154
acailly <ylliac14@gmail.com>
2021-01-31 15:35:42 +0100

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,5 @@
 const version = "v1";
+const OFFLINE_URL = "index.html";
 
 // Declare filesToCache variable
 self.importScripts("./filesToCache.js");
@@ -6,8 +7,8 @@ self.importScripts("./filesToCache.js");
 // Declare serviceWorkerConfiguration variable
 self.importScripts("./serviceworker-configuration.js");
 
-const baseUrl = serviceWorkerConfiguration.baseUrl;
-console.log("[service-worker] Base URL is", baseUrl);
+const baseURL = serviceWorkerConfiguration.baseURL;
+console.log("[service-worker] base URL is", baseURL);
 
 self.addEventListener("install", function (event) {
   console.log("[service-worker] installation");
@@ -23,56 +24,11 @@ self.addEventListener("install", function (event) {
   event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("[service-worker] activation");
-});
-
-self.addEventListener("fetch", async function (event) {
-  // TL;DR; Strategy: Cache falling back to app falling back the network
-
-  event.respondWith(
-    // Start searching in the cache...
-    caches
-      .match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-
-        //... if not found, look if it is an URL of an app page
-        if (event.request.url.startsWith(baseUrl)) {
-          return caches.match(`${baseUrl}index.html`);
-        }
-      })
-      //... else fallback to the network
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-      .catch((e) => {
-        //... if this is an error in cors mode, retry with a CORS proxy
-        if (event.request.mode === "cors") {
-          const corsProxifiedURL = `${serviceWorkerConfiguration.corsProxyURL}${event.request.url}`;
-          // From https://stackoverflow.com/a/35421858
-          const proxifiedRequest = new Request(corsProxifiedURL, {
-            method: event.request.method,
-            headers: event.request.headers,
-            mode: event.request.mode,
-            credentials: event.request.credentials,
-            redirect: event.request.redirect,
-          });
-          return fetch(proxifiedRequest);
-        }
-
-        throw e;
-      })
-  );
-});
-
 self.addEventListener("activate", function (event) {
+  console.log("[service-worker] activation");
+
   // Remove old caches
+  console.log("[service-worker] clean old cache");
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
       return Promise.all(
@@ -88,5 +44,91 @@ self.addEventListener("activate", function (event) {
   );
 
   // Set this service worker as clients' active service worker
+  console.log("[service-worker] claim as active service worker");
   event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", async function (event) {
+  // TL;DR; Strategy: Cache falling back to app falling back the network
+
+  console.log("[service-worker]", event.request.url, " - intercepted");
+
+  event.respondWith(
+    Promise.resolve(true)
+      // Start searching in the cache...
+      .then(() => {
+        console.log(
+          "[service-worker]",
+          event.request.url,
+          "- try to load from cache"
+        );
+
+        return caches.match(event.request);
+      })
+      .then((response) => {
+        if (response) {
+          console.log(
+            "[service-worker]",
+            event.request.url,
+            "- loaded from cache!"
+          );
+          return response;
+        }
+
+        //... else fallback to the network
+        console.log(
+          "[service-worker]",
+          event.request.url,
+          "- fallback to network"
+        );
+        return fetch(event.request);
+      })
+      .catch((e) => {
+        console.log("[service-worker]", event.request.url, "- failed!");
+
+        //... if this is an error in cors mode, retry with a CORS proxy
+        if (event.request.mode === "cors") {
+          console.log(
+            "[service-worker]",
+            event.request.url,
+            " - retry with CORS proxy"
+          );
+
+          const corsProxifiedURL = `${serviceWorkerConfiguration.corsProxyURL}${event.request.url}`;
+          console.log(
+            "[service-worker]",
+            event.request.url,
+            " - proxyfied URL:",
+            corsProxifiedURL
+          );
+
+          // From https://stackoverflow.com/a/35421858
+          const proxifiedRequest = new Request(corsProxifiedURL, {
+            method: event.request.method,
+            headers: event.request.headers,
+            mode: event.request.mode,
+            credentials: event.request.credentials,
+            redirect: event.request.redirect,
+          });
+
+          return fetch(proxifiedRequest);
+        }
+
+        //... if this is an error in navigate mode, this is likely due to
+        // a network failure, so we load the app landing page from the cache
+        // (inspired from https://web.dev/offline-fallback-page/)
+        if (event.request.mode === "navigate") {
+          console.log(
+            "[service-worker]",
+            event.request.url,
+            "- try to fallback to app offline landing page"
+          );
+          return caches.match(`${OFFLINE_URL}`);
+        }
+
+        //... else, just log error :-/
+        console.error("[service-worker]", event.request.url, " - error:", e);
+        throw e;
+      })
+  );
 });
