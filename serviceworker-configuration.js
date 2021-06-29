f6398c7d3861cecbf0952edfbfe19d1cea916068
acailly <ylliac14@gmail.com>
2021-06-29 21:53:29 +0200

    Updates

--- serviceworker-configuration.js
+++ serviceworker-configuration.js
@@ -1,5 +1,5 @@
 var serviceWorkerConfiguration = {
   "baseURL": "/nouvel",
   "corsProxyURL": "https://acailly-cors-anywhere.herokuapp.com/",
-  "applicationVersion": "v1-beta05"
+  "applicationVersion": "v1-beta06"
 }
